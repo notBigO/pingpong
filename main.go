e2392ad1374d0b4c556c894fa32be77c623e19ea
notBigO <2003varunp@gmail.com>
2024-08-26 17:08:02 +0530

    init

---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/libp2p/go-libp2p"
+	peerstore "github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/p2p/protocol/ping"
+	"github.com/multiformats/go-multiaddr"
+)
+
+func main() {
+	// node, err := libp2p.New()
+	node, err := libp2p.New(
+		libp2p.ListenAddrStrings("/ip4/127.0.0.1/tcp/0"),
+		libp2p.Ping(false),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	pingService := &ping.PingService{Host: node}
+	node.SetStreamHandler(ping.ID, pingService.PingHandler)
+
+	peerInfo := peerstore.AddrInfo{
+		ID:    node.ID(),
+		Addrs: node.Addrs(),
+	}
+
+	addrs, err := peerstore.AddrInfoToP2pAddrs(&peerInfo)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("libp2p node address:", addrs[0])
+
+	if len(os.Args) > 1 {
+		addr, err := multiaddr.NewMultiaddr(os.Args[1])
+		if err != nil {
+			panic(err)
+		}
+
+		peer, err := peerstore.AddrInfoFromP2pAddr(addr)
+		if err != nil {
+			panic(err)
+		}
+
+		if err := node.Connect(context.Background(), *peer); err != nil {
+			ch := pingService.Ping(context.Background(), peer.ID)
+			for i := 0; i < 5; i++ {
+				res := <-ch
+				fmt.Println("Got ping response!", "RTT: ", res.RTT)
+			}
+		}
+	} else {
+		ch := make(chan os.Signal, 1)
+		signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+		<-ch
+		fmt.Println("Received signal, shutting down...")
+	}
+
+	if err := node.Close(); err != nil {
+		panic(err)
+	}
+}
